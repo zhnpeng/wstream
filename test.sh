520961dae0d2c1a665babc131893e0fd97bb5344
zhenpeng <lizhen2096@163.com>
2018-08-28 15:32:23 +0800

    add global test.sh

---
+++ test.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+go clean -r
+# TODO: need to ignore some folder
+# go test -test.short -v ./... | sed ''/PASS/s//$(printf "\033[32mPASS\033[0m")/'' | sed ''/FAIL/s//$(printf "\033[31mFAIL\033[0m")/''
+go test -test.short -v ./flow/... | sed ''/PASS/s//$(printf "\033[32mPASS\033[0m")/'' | sed ''/FAIL/s//$(printf "\033[31mFAIL\033[0m")/''
+go test -test.short -v ./runtime/... | sed ''/PASS/s//$(printf "\033[32mPASS\033[0m")/'' | sed ''/FAIL/s//$(printf "\033[31mFAIL\033[0m")/''
+go test -test.short -v ./helpers/... | sed ''/PASS/s//$(printf "\033[32mPASS\033[0m")/'' | sed ''/FAIL/s//$(printf "\033[31mFAIL\033[0m")/''
+go test -test.short -v ./types/... | sed ''/PASS/s//$(printf "\033[32mPASS\033[0m")/'' | sed ''/FAIL/s//$(printf "\033[31mFAIL\033[0m")/''
