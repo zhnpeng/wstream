b31c942abc6d6516b8958a53562df6b03a74f8e7
zhenpeng <lizhen2096@163.com>
2018-10-30 18:22:22 +0800

    ci

--- test.sh
+++ test.sh
@@ -1,2 +1,13 @@
 #!/usr/bin/env bash
-go test  -test.short -p=1 -v ./... | sed ''/PASS/s//$(printf "\033[32mPASS\033[0m")/'' | sed ''/FAIL/s//$(printf "\033[31mFAIL\033[0m")/''
+
+set -e
+echo "" > coverage.txt
+
+for d in $(go list ./... \
+	| grep -v vendor); do
+    go test -race -coverprofile=profile.out -covermode=atomic $d
+    if [ -f profile.out ]; then
+        cat profile.out >> coverage.txt
+        rm profile.out
+    fi
+done
