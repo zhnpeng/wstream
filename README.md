02e790b2adaf3b1165b9cd94549295edb78bb7b6
zhenpeng <lizhen2096@163.com>
2018-11-03 00:06:00 +0800

    add example and refine reamdme

--- README.md
+++ README.md
@@ -2,4 +2,47 @@
 [![Build Status](https://travis-ci.org/wandouz/wstream.svg?branch=master)](https://travis-ci.org/wandouz/wstream)
 [![codecov](https://codecov.io/gh/wandouz/wstream/branch/master/graph/badge.svg)](https://codecov.io/gh/wandouz/wstream)
 
-infinity streaming processing written in golang
+streaming processing API for golang
+
+## Install
+``` console
+go get github.com/wandouz/wstream
+```
+
+## Supported Operator
+
+* Map
+* FlatMap
+* Filter
+* Reduce
+* KeyBy
+* TimeWindow
+* SlidingTimeWindow
+* CountWindow
+* SlidingCountWindow
+* Window.Reduce
+* Window.Apply
+
+## Usage
+
+```go
+func main() {
+    flow, source := stream.New("tumbling_event_window")
+    source.MapChannels(input1, input2). //two map[string]interface{} channels as input
+        AssignTimeWithPuncatuatedWatermark( // assign time and generate watermark
+            &myAssignTimeWithPuncatuatedWatermark{},
+        ).
+        Map(&myMapFunc{}).
+        KeyBy("Key1", "Key2"). // group by "Key1" and "Key2"
+        TimeWindow(2). // tumblint event time window for every 2 seconds
+        Reduce(&myReduceFunc{}). // reduce on all datas for each window
+        Output(outfunc)
+    flow.Run() // Run infinity until input1 and input2 are closed
+}
+```
+
+## Examples
+
+### Reduce with tumbling event time window
+
+[full example](examples/windows/tumbling_time_window.go)
---
+++ examples/windows/tumbling_time_window.go
@@ -0,0 +1,217 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+
+	"github.com/spf13/cast"
+	"github.com/wandouz/wstream/stream"
+	"github.com/wandouz/wstream/types"
+	"github.com/wandouz/wstream/utils"
+)
+
+type myMapFunc struct{}
+
+func (tmf *myMapFunc) Map(r types.Record) (o types.Record) {
+	x := cast.ToInt(r.Get("X"))
+	r.Set("X", x+1)
+	return r
+}
+
+type myReduceFunc struct{}
+
+func (trf *myReduceFunc) Reduce(a, b types.Record) types.Record {
+	if a == nil {
+		return types.NewRawMapRecord(
+			map[string]interface{}{
+				"X": cast.ToInt(b.Get("X")),
+			},
+		)
+	}
+	x := cast.ToInt(a.Get("X")) + cast.ToInt(b.Get("X"))
+	return types.NewRawMapRecord(
+		map[string]interface{}{
+			"X": x,
+		},
+	)
+}
+
+type myOutputFunc struct {
+}
+
+func (t *myOutputFunc) Output(r types.Record) {
+	fmt.Println(r)
+}
+
+type myAssignTimeWithPuncatuatedWatermark struct {
+}
+
+func (t *myAssignTimeWithPuncatuatedWatermark) ExtractTimestamp(r types.Record, pts int64) int64 {
+	return cast.ToInt64(r.Get("T"))
+}
+
+func (t *myAssignTimeWithPuncatuatedWatermark) GetNextWatermark(r types.Record, ets int64) (wm *types.Watermark) {
+	isWatermark := cast.ToBool(r.Get("Watermark"))
+	if isWatermark {
+		return types.NewWatermark(time.Unix(ets, 0))
+	}
+	return
+}
+
+func main() {
+	input1 := make(chan map[string]interface{})
+	input2 := make(chan map[string]interface{})
+	flow, source := stream.New("tumbling_event_window")
+	outfunc := &myOutputFunc{}
+	source.MapChannels(input1, input2).
+		AssignTimeWithPuncatuatedWatermark(&myAssignTimeWithPuncatuatedWatermark{}).
+		Map(&myMapFunc{}).
+		KeyBy("D1", "D2").
+		TimeWindow(2).
+		Reduce(&myReduceFunc{}).
+		Output(outfunc)
+
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		for _, m := range dataset1 {
+			input1 <- m
+		}
+		close(input1)
+	}()
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		for _, m := range dataset2 {
+			input2 <- m
+		}
+		close(input2)
+	}()
+	flow.Run()
+	wg.Wait()
+}
+
+var (
+	dataset1 = []map[string]interface{}{
+		map[string]interface{}{
+			"Watermark": true,
+			"T":         utils.ParseTime("2018-10-15 18:00:00").Unix(),
+			"D1":        "A",
+			"D2":        "A",
+			"X":         1,
+		},
+		map[string]interface{}{
+			"T":  utils.ParseTime("2018-10-15 18:00:01").Unix(),
+			"D1": "A",
+			"D2": "A",
+			"X":  1,
+		},
+		map[string]interface{}{
+			"T":  utils.ParseTime("2018-10-15 18:00:00").Unix(),
+			"D1": "A",
+			"D2": "B",
+			"X":  1,
+		},
+		map[string]interface{}{
+			"T":  utils.ParseTime("2018-10-15 18:00:01").Unix(),
+			"D1": "B",
+			"D2": "B",
+			"X":  1,
+		},
+		map[string]interface{}{
+			"Watermark": true,
+			"T":         utils.ParseTime("2018-10-15 18:00:02").Unix(),
+			"D1":        "A",
+			"D2":        "B",
+			"X":         1,
+		},
+		map[string]interface{}{
+			"T":  utils.ParseTime("2018-10-15 18:00:02").Unix(),
+			"D1": "A",
+			"D2": "B",
+			"X":  1,
+		},
+		map[string]interface{}{
+			"T":  utils.ParseTime("2018-10-15 18:00:02").Unix(),
+			"D1": "A",
+			"D2": "A",
+			"X":  1,
+		},
+		map[string]interface{}{
+			"T":  utils.ParseTime("2018-10-15 18:00:03").Unix(),
+			"D1": "A",
+			"D2": "A",
+			"X":  1,
+		},
+		map[string]interface{}{
+			"Watermark": true,
+			"T":         utils.ParseTime("2018-10-15 18:00:04").Unix(),
+			"D1":        "C",
+			"D2":        "C",
+			"X":         1,
+		},
+	}
+
+	dataset2 = []map[string]interface{}{
+		map[string]interface{}{
+			"Watermark": true,
+			"T":         utils.ParseTime("2018-10-15 18:00:00").Unix(),
+			"D1":        "B",
+			"D2":        "B",
+			"X":         1,
+		},
+		map[string]interface{}{
+			"T":  utils.ParseTime("2018-10-15 18:00:01").Unix(),
+			"D1": "A",
+			"D2": "B",
+			"X":  1,
+		},
+		map[string]interface{}{
+			"T":  utils.ParseTime("2018-10-15 18:00:00").Unix(),
+			"D1": "A",
+			"D2": "B",
+			"X":  1,
+		},
+		map[string]interface{}{
+			"Watermark": true,
+			"T":         utils.ParseTime("2018-10-15 18:00:01").Unix(),
+			"D1":        "B",
+			"D2":        "B",
+			"X":         1,
+		},
+		map[string]interface{}{
+			"Watermark": true,
+			"T":         utils.ParseTime("2018-10-15 18:00:02").Unix(),
+			"D1":        "A",
+			"D2":        "A",
+			"X":         1,
+		},
+		map[string]interface{}{
+			"T":  utils.ParseTime("2018-10-15 18:00:02").Unix(),
+			"D1": "A",
+			"D2": "B",
+			"X":  1,
+		},
+		map[string]interface{}{
+			"T":  utils.ParseTime("2018-10-15 18:00:02").Unix(),
+			"D1": "B",
+			"D2": "B",
+			"X":  1,
+		},
+		map[string]interface{}{
+			"T":  utils.ParseTime("2018-10-15 18:00:03").Unix(),
+			"D1": "B",
+			"D2": "A",
+			"X":  1,
+		},
+		map[string]interface{}{
+			"Watermark": true,
+			"T":         utils.ParseTime("2018-10-15 18:00:04").Unix(),
+			"D1":        "C",
+			"D2":        "C",
+			"X":         1,
+		},
+	}
+)
