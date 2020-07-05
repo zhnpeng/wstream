851f893b234b94119ebc61d730813eb30a5fa0fe
jasper.li <jasper.li@netis.com.cn>
2020-07-06 00:53:42 +0800

    TBC

---
+++ runtime/operator/windowing/timer/structures.go
@@ -0,0 +1,49 @@
+package timer
+
+import (
+	"time"
+
+	"github.com/zhnpeng/wstream/runtime/operator/windowing"
+)
+
+type TimerHeap []TimerHeapItem
+
+type TimerHeapItem struct {
+	t   time.Time
+	wid windowing.WindowID
+}
+
+func (t *TimerHeapItem) Time() time.Time {
+	return t.t
+}
+
+func (t *TimerHeapItem) WID() windowing.WindowID {
+	return t.wid
+}
+
+func (h TimerHeap) Len() int { return len(h) }
+
+// Less function for heap sort by Time
+func (h TimerHeap) Less(i, j int) bool {
+	return h[i].t.Before(h[j].t)
+}
+
+func (h TimerHeap) Swap(i, j int) {
+	h[i], h[j] = h[j], h[i]
+}
+
+func (h TimerHeap) Top() TimerHeapItem {
+	return h[0]
+}
+
+func (h *TimerHeap) Push(x interface{}) {
+	*h = append(*h, x.(TimerHeapItem))
+}
+
+func (h *TimerHeap) Pop() interface{} {
+	old := *h
+	n := len(old)
+	x := old[n-1]
+	*h = old[0 : n-1]
+	return x
+}
---
+++ runtime/operator/windowing/timer/timer.go
@@ -0,0 +1,18 @@
+package timer
+
+import (
+	"time"
+
+	"github.com/zhnpeng/wstream/runtime/operator/windowing"
+)
+
+type Timer interface {
+	// OnTime means OnProcessingTime or OnEventTime
+	OnTime(t time.Time)
+	// Register a window to timmer
+	RegisterWindow(wid windowing.WindowID, t time.Time)
+	// CurrentTime() return current processing time or event time
+	CurrentTime() time.Time
+	Start()
+	Stop()
+}
