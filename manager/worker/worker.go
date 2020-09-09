42784e980ceda8e09c5f79ae3c57da85b252e8d6
jasper.li <jasper.li@netis.com.cn>
2020-09-09 22:36:56 +0800

    +

--- manager/worker/worker.go
+++ manager/worker/worker.go
@@ -2,17 +2,22 @@ package worker
 
 import (
 	"github.com/coreos/etcd/clientv3"
+	sdetcdv3 "github.com/go-kit/kit/sd/etcdv3"
 	"gopkg.in/tomb.v1"
 )
 
 type Worker interface {
-	Register() error
-	Deregister() error
+	Key() string
+	Endpoint() string
 	Bind() error
 	Service() (Service, error)
 	Run()
 	Wait() error
 	Kill(reason error)
+
+	// service discovery
+	Register() error
+	Deregister() error
 }
 
 var WorkerKeyPrefix = "/wstream/worker/"
@@ -21,20 +26,27 @@ type worker struct {
 	client   *clientv3.Client
 	endpoint string
 
+	registrar sdetcdv3.Registrar
 	tomb.Tomb
 }
 
 func NewWorker() *worker {
+	// TODO: construct registrar
+	// reg = sdetcdv3.NewRegistrar()
 	// TODO: construct worker object
 	return &worker{}
 }
 
 func (w *worker) Run() {
-
 }
 
-func (w *worker) Register() error {
+// Register service at startup
+func (w *worker) Register() {
+	w.registrar.Register()
+}
 
+func (w *worker) Deregister() {
+	w.registrar.Deregister()
 }
 
 func (w *worker) Service() (Service, error) {
